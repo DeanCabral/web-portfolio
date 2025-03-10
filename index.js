6667db5bb498cb1a697cea09da2b71c4937376ef
Dean Cabral <26599041+DeanCabral@users.noreply.github.com>
2025-03-10 14:43:15 +0000

    Updated menu layout

--- index.js
+++ index.js
@@ -21,15 +21,18 @@ window.onload = () => {
 
 window.onscroll = () => {
   const navHeader = document.getElementById("header");
+  const navMenu = document.getElementById("nav-menu-res");
 
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
     navHeader.style.height = "70px";
     navHeader.style.lineHeight = "70px";
+    navMenu.style.top = "65px";
   } else {
     navHeader.style.boxShadow = "none";
     navHeader.style.height = "90px";
     navHeader.style.lineHeight = "90px";
+    navMenu.style.top = "90px";
   }
 }
 
