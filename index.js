899b7eae043c221ae0c0d97007037749777ee45f
Dean Cabral <26599041+DeanCabral@users.noreply.github.com>
2025-04-02 20:45:18 +0100

    Update boxshadow

--- index.js
+++ index.js
@@ -87,12 +87,10 @@ window.onscroll = () => {
   const scrollPosition = document.body.scrollTop || document.documentElement.scrollTop;
 
   if (scrollPosition > 50) {
-      navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
       navHeader.style.height = "70px";
       navHeader.style.lineHeight = "70px";
       navMenu.style.top = "65px";
   } else {
-      navHeader.style.boxShadow = "none";
       navHeader.style.height = "90px";
       navHeader.style.lineHeight = "90px";
       navMenu.style.top = "90px";
